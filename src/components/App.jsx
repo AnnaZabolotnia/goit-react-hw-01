ffca9ec7526f232ebac07b2b58988645f30b0f03
AnnaZabolotnia <137837327+AnnaZabolotnia@users.noreply.github.com>
2024-03-11 19:24:49 +0200

    goit-react-hw-01

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import Profile from "./Profile/Profile";
+import userData from "../assets/userData.json";
+import friendsData from "../assets/friendsData.json";
+import FriendList from "./Friendslist/Friendslist";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
+import transactions from "../assets/transactions.json";
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friendsData} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+}
---
+++ src/components/Friendslist/Friendslist.jsx
@@ -0,0 +1,22 @@
+import css from "./friendlist.module.css";
+import FriendListItem from "./FriendListItem/FriendListItem";
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendsList}>
+      {friends?.map((friend) => {
+        return (
+          <li key={friend.id} className={css.friendsCard}>
+            <FriendListItem
+              avatar={friend.avatar}
+              name={friend.name}
+              isOnline={friend.isOnline}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default FriendList;
